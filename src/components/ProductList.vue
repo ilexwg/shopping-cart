31150d3ff6649adaf8b9324683f45bca0a208aab
ilexwg <ilexwg@gmail.com>
2021-05-13 01:35:24 +0800

    done

---
+++ src/components/ProductList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <img
+      v-if="loading"
+      src="https://i.imgur.com/JfPpwOA.gif"
+      alt="Loading..."
+    />
+    <ul v-else>
+      <li v-for="product of products" :key="product.id">
+        {{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
+        <button
+          :disabled="!productIsInStock(product)"
+          @click="addProductToCart(product)"
+        >Add to cart</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ProductList',
+
+  data() {
+    return {
+      loading: false,
+    };
+  },
+
+  computed: {
+    ...mapState('products', {
+      products: state => state.items
+    }),
+    ...mapGetters('products', {
+      productIsInStock: 'productIsInStock'
+    }),
+  },
+
+  created() {
+    this.loading = true;
+    this.fetchProducts()
+      .then(() => (this.loading = false))
+  },
+
+  methods: {
+    ...mapActions({
+      fetchProducts: 'products/fetchProducts',
+      addProductToCart: 'cart/addProductToCart'
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
